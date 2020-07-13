61ea247e8dc24f774946d90efd2fe9f0a660278d
Willy <williamvn1995@gmail.com>
2020-07-13 18:47:01 +0200

    Data Service Structure Done

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from "@angular/common/http";
+import { User } from './model/user';
+import { Professional } from './model/professional';
+import { exception } from 'console';
+import { Patient } from './model/patient';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private users;
+  private _professionals :Professional[];
+  private _patients :Patient[];
+  
+  get professionals(){
+    return this._professionals;
+  }
+
+  get patients(){
+    return this._patients;
+  }
+
   constructor() { }
+
+  /**
+   * Load from the API all the professionals and patients
+   */
+  public loadUsers():void {
+    //Use Fork Join
+  }
+
+  private loadProfessionals():Professional[]{
+    //fetch professionals
+    throw new exception("Not Implemented");
+  }
+
+  private loadPatients():Patient[]{
+    //fetch Patients;
+    throw new exception("Not Implemented");
+  }
+
 }
