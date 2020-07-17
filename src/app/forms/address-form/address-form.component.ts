16d7c488bdc5d813f5cd1692d9f2b8ea99b11ca3
Willy <williamvn1995@gmail.com>
2020-07-17 10:23:38 +0200

    AutoComplete City Add

--- src/app/forms/address-form/address-form.component.ts
+++ src/app/forms/address-form/address-form.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { FormsService } from 'src/app/services/forms.service';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss' ,'../../user-details/user-details.component.scss']
+  styleUrls: ['./address-form.component.scss' ,'../../user-details/user-details.component.scss'],
+  providers:[FormsService]
 })
 export class AddressFormComponent implements OnInit {
 
   @Input() isEditable: boolean = true;
   @Input() addressFormGroup: FormGroup;
-  constructor() { }
+  cities:string[] = [];
+  filteredOptions: Observable<string[]>;
+  constructor(private formService: FormsService) { }
 
   ngOnInit(): void {
+    this.formService.loadCities().subscribe((cities) => {
+      this.cities = cities;
+    });
+    this.filteredOptions = this.addressFormGroup.get("city").valueChanges.pipe(
+      startWith(''),
+      map(value => this.filter(value))
+    );
+  }
+
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.cities.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
 }
--- src/app/services/forms.service.ts
+++ src/app/services/forms.service.ts
@@ -1,6 +1,9 @@
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '../model/user';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class FormsService {
@@ -16,7 +19,7 @@ export class FormsService {
   isProfessional: boolean = true;
   isEditable: boolean = true;
 
-  constructor(private formbuilder: FormBuilder) { }
+  constructor(private formbuilder: FormBuilder, private http: HttpClient) { }
 
   public createProfessionalForm(): void {
     this.createUserForm();
@@ -28,7 +31,7 @@ export class FormsService {
   
   }
 
-  createUserForm(): void {
+  private createUserForm(): void {
     this.personalInfoForm = this.formbuilder.group({
       name: [this.user.name, Validators.required],
       firstName: [this.user.firstName, Validators.required],
@@ -39,7 +42,7 @@ export class FormsService {
     });
   }
 
-  createAddressForm(): void {
+  private createAddressForm(): void {
     this.addressForm = this.formbuilder.group({
       street: [this.user.address.street],
       door: [this.user.address.door],
@@ -49,7 +52,7 @@ export class FormsService {
     })
   }
 
-  createPatientForm(): void {
+  public createPatientForm(): void {
     this.createUserForm();
 
     this.createAddressForm();
@@ -90,5 +93,11 @@ export class FormsService {
     else {
       this.user[property] = form.getRawValue();
     }
+  }
+
+  public loadCities():Observable<string[]>{
+    return this.http.get("../../assets/cities.json").pipe(map((res)=>{
+      return res["cities"];
+    }));
   }
 }
