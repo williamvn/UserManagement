28db4fbdcd9a4f9dd8b42dbb338989613c223949
Willy <williamvn1995@gmail.com>
2020-07-28 00:30:55 +0200

    Login Done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppUser } from '../model/app-user';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { AppUserService } from '../services/app-user.service';
 
 
 @Component({
@@ -11,16 +12,29 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class LoginComponent implements OnInit {
 
+  errorMessage: string = "";
   user: AppUser = new AppUser();
-  constructor(private router: Router, private _snackBar: MatSnackBar) { }
+  constructor(private router: Router, private _snackBar: MatSnackBar, private appUserService: AppUserService) { }
 
   ngOnInit(): void {
   }
 
   logUser() {
-    this._snackBar.open("Entrada Exitosa", "Aceptar", {
-      duration: 2000,
+    this.appUserService.LogUser(this.user).subscribe((success) => {
+      if (success) {
+        this._snackBar.open("Entrada Exitosa", "Aceptar", {
+          duration: 2000,
+        });
+        this.router.navigate(["users"]);
+      }
+    }, (error) => {
+      // console.log(error); 
+      if (error.status === 401) {
+        this.errorMessage = "Nombre de usuario o contraseña incorrecta";
+      }
+      else {
+        this.router.navigate(["error"]);
+      }
     });
-    this.router.navigate(["users"]);
   }
 }
---
+++ src/app/services/app-user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { AppUser } from '../model/app-user';
+import { HttpClient } from '@angular/common/http';
+import { TokenResponse } from '../DTO/token-response';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppUserService {
+    BASE_URI = "http://192.168.0.164:3000/auth/login";
+
+    private _token: string;
+
+
+    get token() {
+        return this._token;
+    }
+
+    constructor(private http: HttpClient) { }
+
+    /**
+     * Log the User and Get Token for Auth
+     */
+    public LogUser(appUser: AppUser): Observable<boolean> {
+        return this.http.post<TokenResponse>(this.BASE_URI, appUser)
+            .pipe(
+                map((response: TokenResponse) => {
+                    this._token = response.accessToken;
+                    return true;
+                }));
+    }
+}
